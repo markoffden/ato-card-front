37aa6fb5f8b4bab8db5d06e830d154fbf3a10bd0
Denys Markov <denys.markov@n3wnormal.com>
2017-07-14 00:53:24 +0300

    custom validators FormGroup edit

--- src/app/shared/custom-validators.ts
+++ src/app/shared/custom-validators.ts
@@ -1,9 +1,9 @@
-import {FormControl, AbstractControl} from "@angular/forms";
+import {FormGroup} from "@angular/forms";
 
 export class CustomValidators {
 
     static matchValue(matchControlName: string, matchAgainstControlName: string) {
-        return (control: FormControl) => {
+        return (control: FormGroup) => {
             let matchControl = control.get(matchControlName).value;
             let matchAgainstControl = control.get(matchAgainstControlName).value;
             if (matchControl !== matchAgainstControl) {
